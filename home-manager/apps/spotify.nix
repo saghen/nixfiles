5dcf578378cf32fc6cad2af745421c0906545c9a
Liam Dyer <liamcdyer@gmail.com>
2026-02-24 02:26:06 -0500

    fix: switch spotify genre plugins

--- home-manager/apps/spotify.nix
+++ home-manager/apps/spotify.nix
@@ -18,18 +18,7 @@ in
     enabledExtensions = with spicePkgs.extensions; [
       hidePodcasts
       bookmark
-      {
-        src =
-          (pkgs.fetchFromGitHub {
-            owner = "LucasOe";
-            repo = "spicetify-genres";
-            rev = "b833f2d94ea6c59262f433857083e377b2522b52";
-            hash = "sha256-sGmqgL+UmFw587yfBHWT/UvcOI7+TkXb+IOFZGB+reo=";
-          })
-          + /dist;
-
-        name = "whatsThatGenre.js";
-      }
+      betterGenres
     ];
   };
 }
